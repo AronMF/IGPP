68f0a858640d949e789b08c4241516d4c0926636
Aron Mamani <aronmamani@Arons-MacBook-Pro.local>
2019-08-11 16:33:37 -0300

    IGPP init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Cointerner html  
+    <home-component></home-component>
+    
+    <maps-component></maps-component>
+  </div>
+</template>
+<script>
+import HomeComponent from './components/Home'
+import MapsComponent from './components/Maps'
+export default {
+  components:{
+    HomeComponent,
+    MapsComponent
+  }
+  
+}
+</script>
---
+++ src/components/Maps.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        Maps
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d924.0010695015987!2d-51.38075617080894!3d-22.125808456743684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9493f5afc69f0a15%3A0xf35565d7d97b5cc2!2sR.+Bahia%2C+435+-+Vila+Marcondes%2C+Pres.+Prudente+-+SP%2C+19030-030!5e0!3m2!1ses!2sbr!4v1565549185859!5m2!1ses!2sbr" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+</template>
